b5e0156da75f3350863b18c6c58ecbe980103e81
k16blfurm <43151076+k16blfurm@users.noreply.github.com>
2019-08-02 22:01:41 -1000

    Speaker sketch going up to 1000 K using pin 13/GND

---
+++ Speaker_sketch/Speaker_sketch.ino
@@ -0,0 +1,29 @@
+int speaker_pin = 13; 
+int led = 12;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(speaker_pin, OUTPUT);
+      
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int speaker = 0;
+  float sweep = 100;
+  int frequency = 10;
+  bool TRUE = true;
+//  pinMode(speaker_pin, OUTPUT);
+
+  while(TRUE == true)
+  {
+  tone(speaker_pin,frequency);
+  delay(sweep);
+  frequency = frequency + 10;
+  tone(speaker_pin,frequency);
+  if(frequency >= 5000)
+  frequency = 10;
+
+  }
+}
